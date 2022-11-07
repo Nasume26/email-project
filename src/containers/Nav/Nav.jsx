dfac494701db7d777bc891e53d7da99542fd8723
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-11-07 13:11:11 -0600

    More Nav edits

--- src/containers/Nav/Nav.jsx
+++ src/containers/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import "./Nav.scss"
 import logo from "../../data/images/logo.webp"
+import profile from "../../data/images/profile.jpeg"
 const Nav = (props) => {
 
     return (
@@ -9,7 +10,8 @@ const Nav = (props) => {
                 <h1>Relay<span className="nav__logo__io">.io</span></h1>
             </div>
             <div className="nav__profile">
-                TEST
+                <img src= {profile} alt = "profile picture" />
+                .
             </div>
         </div>
     )
