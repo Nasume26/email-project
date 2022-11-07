408d83fe4214e35d8e894e92f5aae76df1cc654c
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-11-07 12:38:00 -0600

    Data template created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import "./App.scss";
 
 function App() {
+  // Importance key 1: low, 2: medium, 3: high
+  // Type key 1: personal, 2: work
+  const dataArr= [
+    {
+      body: "Test",
+      importance: 1,
+      date: "11/21/2016",
+      type: 1,
+      isRead: false
+    }
+  ]
+
+
+
+
+
   return (
     <div className="App">
       <h1>Test</h1>
