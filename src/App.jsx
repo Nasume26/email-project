7026ef81f587707092a0c3b8d5dbbd8e956e52cd
Maxwell Roberson <108200270+Nasume26@users.noreply.github.com>
2022-11-07 12:59:20 -0600

    Began working on nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,39 @@
 import "./App.scss";
+import { useState, useEffect } from "react";
+import Nav from "./containers/Nav/Nav";
 
 function App() {
   // Importance key 1: low, 2: medium, 3: high
   // Type key 1: personal, 2: work
   const dataArr= [
     {
+      name: "Test Man",
+      email: "test@test.com",
       body: "Test",
       importance: 1,
       date: "11/21/2016",
       type: 1,
-      isRead: false
+      isRead: false,
+      isTrash: false
+    },    
+    {
+      name: "Other Test",
+      email: "test@test.com",
+      body: "Test 2",
+      importance: 3,
+      date: "10/22/2017",
+      type: 2,
+      isRead: false,
+      isTrash: false
     }
+
   ]
-
-
 
 
 
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Nav />
     </div>
   );
 }
---
+++ src/containers/Nav/Nav.jsx
@@ -0,0 +1,18 @@
+import "./Nav.scss"
+import logo from "../../data/images/logo.webp"
+const Nav = (props) => {
+
+    return (
+        <div className="nav">
+            <div className="nav__logo">
+                <img src = {logo} alt = "logo" />
+                <h1>Relay.io</h1>
+            </div>
+            <div className="nav__profile">
+                TEST
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
